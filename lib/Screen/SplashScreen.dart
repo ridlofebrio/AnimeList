1dfac4bb2cabbb9b1d45829773b899af7dca9d45
ridlofebrio <muhridlo111@gmail.com>
2024-08-13 13:40:34 +0700

    add splash screen

---
+++ lib/Screen/SplashScreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterSplashScreen.scale(
+      gradient: const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromARGB(255, 253, 176, 60),
+          Color.fromARGB(255, 253, 176, 60),
+        ],
+      ),
+      childWidget: SizedBox(
+        height: 190,
+        child: Image.asset("lib/assets/logo.jpeg"),
+      ),
+      duration: const Duration(milliseconds: 3500),
+      animationDuration: const Duration(milliseconds: 1000),
+      onAnimationEnd: () => debugPrint("On Scale End"),
+      nextScreen: const Scaffold(
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
